437c652ef9106c638204ac2bcecb066c204aac3e
David Willian de Siqueira <david.siqueira-sys@parceiros-sysmap.com.br>
2021-03-09 19:45:17 -0300

    Initial Project

---
+++ ToDoList/ToDoList.Api/Controllers/BoardController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ToDoList.Api.ViewModels;
+
+namespace ToDoList.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BoardController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] BoardViewModel board)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] BoardViewModel board)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ToDoList/ToDoList.Api/ViewModels/BoardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Api.ViewModels
+{
+    public class BoardViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required, Range(1, 30)]
+        public string Name { get; set; }
+    }
+}
